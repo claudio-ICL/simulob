9ab5e72fba2ee8efe07f4a3cd6054d9a69199306
Claudio Bellani <c.bellani17@imperial.ac.uk>
2021-02-21 16:18:58 +0100

    Live sequence
     On branch master
     Your branch is ahead of 'origin/master' by 1 commit.
       (use "git push" to publish your local commits)

--- simulob/src/main.cpp
+++ simulob/src/main.cpp
@@ -12,14 +12,14 @@
 int main() {
   std::shared_ptr<OrderBook> ob =
       std::make_shared<OrderBook>(100, 100000, 100500);
-  double decayrate = 0.000000001;
+  double decayrate = 0.0000001;
   std::shared_ptr<OrderSubmission> osub =
       std::make_shared<OrderSubmission>(ob, decayrate);
-  osub->hawkes.baserate(std::vector<double>{0.001, 0.001});
+  osub->hawkes.baserate(std::vector<double>{0.1, 0.1});
   osub->hawkes.impactcoef(
-      std::vector<std::vector<double>>{{0.05, 0.03}, {0.03, 0.05}});
+      std::vector<std::vector<double>>{{0.5, 0.03}, {0.03, 0.5}});
   osub->hawkes.decaycoef(
-      std::vector<std::vector<double>>{{3.5, 3.5}, {3.5, 3.5}});
+      std::vector<std::vector<double>>{{2.5, 2.5}, {2.5, 2.5}});
   osub->hawkes.set_timeorigin(0.0);
   {
     int status_y = 1;
